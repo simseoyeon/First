1f6370aeb5cb524855ede7bef12ad7a44279107e
seoyeon <simseo034@gmail.com>
2024-06-05 17:50:58 +0900

    "폼 데이터를 받아 게시판 만들기" "게시글 읽기 - 단일 데이터/목록 데이터 조회하기" "게시판 내 페이지 이동하기 - 링크/리다이렉트" "게시글 수정하기: update" "게시글 삭제" "REST API"

--- src/main/java/com/example/firstproject/controller/GroupController.java
+++ src/main/java/com/example/firstproject/controller/GroupController.java
@@ -5,20 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-public class GroupController {
-    @Autowired
-    private GroupService groupService;
-    @Autowired
-    private TeamService teamService;
-
-    @GetMapping("/groups/{groupId}")
-    public String show(@PathVariable Long groupId, Model model){
-        //해당 메서드 작성
-        GroupDto groupDto = groupService.getGroup(groupId);
-        List<TeamDto> teamDtos = teamService.getTeamOnGroup(groupId);
-
-        model.addAttribute("group", groupDto);
-        model.addAttribute("teamDtos", teamDtos);
-        return "groups/show";
-    }
-}
+//public class GroupController {
+//    @Autowired
+//    private GroupService groupService;
+//    @Autowired
+//    private TeamService teamService;
+//
+//    @GetMapping("/groups/{groupId}")
+//    public String show(@PathVariable Long groupId, Model model){
+//        //해당 메서드 작성
+//        GroupDto groupDto = groupService.getGroup(groupId);
+//        List<TeamDto> teamDtos = teamService.getTeamOnGroup(groupId);
+//
+//        model.addAttribute("group", groupDto);
+//        model.addAttribute("teamDtos", teamDtos);
+//        return "groups/show";
+//    }
+//}
